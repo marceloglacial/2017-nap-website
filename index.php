2761bff7b4a28c766c0d38e332653393c9c0f6b0
Marcelo Glacial <glacial@gmail.com>
2017-09-21 11:19:15 -0400

    Change Equipe CSS and design

--- index.php
+++ index.php
@@ -33,45 +33,36 @@
 </div><!-- /Quem -->
 
 <div id="Equipe" class="content">
+
+
   <?php
-  $your_query = new WP_Query( 'pagename=nossa-equipe' );
-  while ( $your_query->have_posts() ) : $your_query->the_post();
+  $args = array(
+    'post_parent' => 7,
+    'post_type' => 'page',
+    'orderby' => 'menu_order'
+  );
+
+  $child_query = new WP_Query( $args );
   ?>
-  <h2 class="text-center"><?php the_title(); ?></h2>
+  <h2 class="text-center">Nossa Equipe</h2> <!-- Fix this -->
   <span class="line"></span>
-  <?php the_content(); ?>
+
+  <?php while ( $child_query->have_posts() ) : $child_query->the_post(); ?>
+
+    <div class="card">
+      <?php
+      if ( has_post_thumbnail() ) {
+        the_post_thumbnail('page-thumb');
+      }
+      ?>
+      <h3><?php the_title(); ?></h3>
+      <?php the_content(); ?>
+    </div>
+  <?php endwhile; ?>
+
   <?php
-endwhile;
-wp_reset_postdata();
-?>
-
-
-<?php
-$args = array(
-  'post_parent' => 7, 
-  'post_type' => 'page',
-  'orderby' => 'menu_order'
-);
-
-$child_query = new WP_Query( $args );
-?>
-
-<?php while ( $child_query->have_posts() ) : $child_query->the_post(); ?>
-
-  <div class="card">
-    <?php
-    if ( has_post_thumbnail() ) {
-      the_post_thumbnail('page-thumb');
-    }
-    ?>
-    <h3><?php the_title(); ?></h3>
-    <?php the_content(); ?>
-  </div>
-<?php endwhile; ?>
-
-<?php
-wp_reset_postdata();
-?>
+  wp_reset_postdata();
+  ?>
 </div><!-- /Equipe -->
 
 <div id="Servicos" class="content">

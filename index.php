6562d68c5ed52ef34539448f66f3a7d21762bac3
Marcelo Glacial <glacial@gmail.com>
2017-09-21 09:34:15 -0400

    Add serviços loop

--- index.php
+++ index.php
@@ -12,36 +12,59 @@
 
 <!-- Content -->
 <section class="container">
-  <div id="Quem" class="content">
 
+  <div id="Quem" class="content">
   <?php
     $your_query = new WP_Query( 'pagename=quem-somos' );
     while ( $your_query->have_posts() ) : $your_query->the_post();
   ?>
-
   <h2><?php the_title(); ?></h2>
   <span class="line"></span>
   <div class="col-md-6">
     <?php the_content(); ?>
   </div>
-
   <?php
     endwhile;
     wp_reset_postdata();
   ?>
-
   <div class="col-md-6">
     <img src="<?php bloginfo('template_url'); ?>/assets/img/bigbluecloud.png" alt="clouds" class="img-responsive">
   </div>
-</div><!-- NOTE: /quem -->
+</div><!-- /Quem -->
+
+<div id="Equipe" class="content">
+  <?php
+    $your_query = new WP_Query( 'pagename=nossa-equipe' );
+    while ( $your_query->have_posts() ) : $your_query->the_post();
+  ?>
+  <h2 class="text-center"><?php the_title(); ?></h2>
+  <span class="line"></span>
+  <?php the_content(); ?>
+  <?php
+    endwhile;
+    wp_reset_postdata();
+  ?>
+</div><!-- /Equipe -->
+
+<div id="Servicos" class="content">
+  <?php
+    $your_query = new WP_Query( 'pagename=nossos-servicos' );
+    while ( $your_query->have_posts() ) : $your_query->the_post();
+  ?>
+  <h2 class="text-center"><?php the_title(); ?></h2>
+  <span class="line"></span>
+  <?php the_content(); ?>
+  <?php
+    endwhile;
+    wp_reset_postdata();
+  ?>
+</div><!-- /Servicos -->
 
 <div id="Contato" class="content text-center">
-
   <?php
     $your_query = new WP_Query( 'pagename=contato' );
     while ( $your_query->have_posts() ) : $your_query->the_post();
   ?>
-
   <h2><?php the_title(); ?></h2>
   <span class="line"></span>
   <?php the_content(); ?>
@@ -49,7 +72,7 @@
     endwhile;
     wp_reset_postdata();
   ?>
-</div><!-- NOTE: /contato -->
+</div><!--  /contato -->
 
 
 </section> <!--Container -->

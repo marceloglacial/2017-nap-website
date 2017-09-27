5784576f37647f34e0d241d1da61f4cc33aefe6c
Marcelo Glacial <glacial@gmail.com>
2017-09-27 18:53:54 -0400

    Remove blog sandwich menu on mobile

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -3,12 +3,14 @@
   <div class="container-fluid">
     <!-- Mobile / Sandwich Menu -->
     <div class="navbar-header <?php if(!is_home()){ ?>brand-center<?php }; ?>">
+      <?php if (is_home()) { ?>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+    <?php }; ?>
       <a class="navbar-brand" href="<?php if(is_home()) { ?> #Top <?php } else { bloginfo('url'); }; ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo_small.png" alt="logo small"></a>
     </div>
 

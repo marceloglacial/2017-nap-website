8dac62e44237df3377dc20c3d8c2481f8672b506
Marcelo Glacial <glacial@gmail.com>
2017-09-28 21:39:50 -0400

    Add float and margin

--- page-blog.php
+++ page-blog.php
@@ -24,7 +24,7 @@
       $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
       $query_args = array(
         'post_type' => 'post',
-        'posts_per_page' => 3,
+        'posts_per_page' => 1,
         'paged' => $paged
       );
       // create a new instance of WP_Query
